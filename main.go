23a7d63864fa50b0ec729dd6e3b82ed521cacd09
Grey <grey0520@foxmail.com>
2023-04-14 20:26:09 +0800

    A simple web program by Go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Grey0520/cb-ai/database"
+	"github.com/Grey0520/cb-ai/router"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	database.ConnectDB()
+
+	router.SetupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ model/model.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+// Book model
+type Book struct {
+	gorm.Model
+
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
